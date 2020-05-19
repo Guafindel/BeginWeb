1bce8664cc03da27b03346bb833eaf7857ce1cb7
Guafindel <50958613+Guafindel@users.noreply.github.com>
2020-05-19 18:06:37 +0900

    0519
    노드 메뉴 동적 구성

--- main.js
+++ main.js
@@ -9,37 +9,58 @@ var app = http.createServer(function (request, response) {
 
     if (pathname === '/') {
         if (queryData.id === undefined) {
-            var title = 'Welcome';
-            var description = 'Hello, Node.js';
-            var template = `
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>WEB1 - ${title}</title>
-                <meta charset="utf-8" />
-            </head>
-            <body>
-                <h1><a href="/">WEB</a></h1>
-                <ul>
-                    <li><a href="/?id=main">Main</a></li>
-                    <li><a href="/?id=who">Who</a></li>
-                    <li><a href="/?id=guest">Guest</a></li>
-                </ul>
-                <h2>${title}</h2>
-                <p>
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/4JJFrjkRxmo" frameborder="0"
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </p>
-                <p>${description}</p>
-            </body>
-            </html>
-            `;
-            response.writeHead(200);
-            response.end(template);
-    } else {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-            var title = queryData.id;
-            var template = `
+            fs.readdir('./data', function (err, fileList) {
+                var title = 'Welcome';
+                var description = 'Hello, Node.js';
+                // var list = `
+                // <ul>
+                //     <li><a href="/?id=main">Main</a></li>
+                //     <li><a href="/?id=who">Who</a></li>
+                //     <li><a href="/?id=guest">Guest</a></li>
+                // </ul>
+                // `;
+
+                var list = '<ul>'
+                // fileList.forEach(element => {
+                //     console.log(element);
+                //     var onTitle = element.substring(0, 1).toUpperCase() + element.substring(1);
+                //     list = list + '<li><a href="?/id=' + element + '>'+ onTitle +'</a></li>';
+                //     list = list + `<li><a href="?/id="${element}">${onTitle}</a></li>`
+                // });
+
+                var i = 0;
+                while(i < fileList.length) {
+                    list = list + `<li><a href="?/id=">${fileList[i]}</a></li>`;
+                    i = i + 1;  
+                }
+                list = list + '</ul>';
+
+                var template = `
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <title>WEB1 - ${title}</title>
+                    <meta charset="utf-8" />
+                </head>
+                <body>
+                    <h1><a href="/">WEB</a></h1>
+                    ${list}
+                    <h2>${title}</h2>
+                    <p>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/4JJFrjkRxmo" frameborder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </p>
+                    <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            })
+        } else {
+            fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
+                var title = queryData.id;
+                var template = `
                 <!DOCTYPE html>
                 <html>
                 <head>
@@ -62,15 +83,15 @@ var app = http.createServer(function (request, response) {
                 </body>
                 </html>
                 `;
-            response.writeHead(200);
-            response.end(template);
-        })
-    }
+                response.writeHead(200);
+                response.end(template);
+            })
+        }
 
-} else {
-    response.writeHead(404);
-    response.end('Not found');
-}
+    } else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
 
 });
 app.listen(3000);
